337486b83b922039d733d8473b56bdd69ef8c1f7
MediaCooper <75029927+MediaCooper@users.noreply.github.com>
2021-02-17 07:22:00 -0800

    Feb.17updates

--- js/data.js
+++ js/data.js
@@ -3,22 +3,22 @@ let data = [
         img: "img/9mm_Winchester_WNUSA9MMVPBX.jpg",
         desc: "Winchester USA 9mm Luger 115 Grain TMJ",
         count: "100 round box",
-        price: "$70"
+        price: "$80"
     }, {
         img: "img/9mm_Aguila_1E097700.jpg",
         desc: "Aguila 9mm Luger 115 Grain FMJ",
         count: "300 round box",
-        price: "$210"
+        price: "$240"
     }, {
         img: "img/9mm_CCI_51991bb.jpg",
         desc: "CCI Blazer Brass 9mm Luger 115 Grain FMJ",
         count: "100 round box",
-        price: "$70"
+        price: "$80"
     }, {
         img: "img/9mm_TSA_TS9MMB.jpg",
         desc: "Target Sports 9mm Luger 124 Grain FMJ",
         count: "50 round box",
-        price: "$35"
+        price: "$40"
     }, {
         img: "img/955112-1.jpg",
         desc: "CCI Blazer .22LR 38 Grain LRN",
