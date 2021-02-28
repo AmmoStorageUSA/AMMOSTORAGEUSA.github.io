eeb1298e16e7f655ca94764d348f57a01546a3e8
MediaCooper <75029927+MediaCooper@users.noreply.github.com>
2021-02-28 07:41:34 -0800

    Feb28Updates

--- js/data.js
+++ js/data.js
@@ -5,16 +5,6 @@ let data = [
         count: "425 round box",
         price: "$90"
     }, {
-        img: "img/40_Winchester_usa40w.jpg",
-        desc: ".40 S&W Winchester USA White Box 165 grain FMJ",
-        count: "100 round box",
-        price: "$80"
-    }, {
-        img: "img/40_Defender.jpg",
-        desc: ".40 S&W Defender 180 grain FMJ",
-        count: "50 round box",
-        price: "$40"
-    }, {
         img: "img/Magazine_Brownells_AR15.jpg",
         desc: "Brownells AR-15 Mil-Spec USGI 5.56/.223 30/round Magazine Magpul Follower & Stainless Steel Spring",
         count: "Single Unit",
