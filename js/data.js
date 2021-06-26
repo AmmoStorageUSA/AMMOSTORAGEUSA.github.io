b10931e1896454ac519165f0905556c2e6f3cf93
MediaCooper <75029927+MediaCooper@users.noreply.github.com>
2021-06-26 17:19:58 -0500

    June26updates

--- js/data.js
+++ js/data.js
@@ -1,10 +1,5 @@
 let data = [
     {
-        img: "img/955112-1.jpg",
-        desc: "CCI Blazer .22LR 38 Grain LRN",
-        count: "425 round box",
-        price: "$90"
-    }, {
         img: "img/Magazine_Brownells_AR15.jpg",
         desc: "Brownells AR-15 Mil-Spec USGI 5.56/.223 30/round Magazine Magpul Follower & Stainless Steel Spring",
         count: "Single Unit",
